27030b12351f24e66dc16294337e8bd731a8a4dd
kevin <kjwilde@hotmail.com>
2018-07-23 16:37:44 -0500

    Draft create

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+#./undeploy.sh
+
 kubectl create namespace cryptocurrency-services
 
 #helm --namespace cryptocurrency-services install -n cryptocurrency-services-api-gateway helm-charts/gateway
