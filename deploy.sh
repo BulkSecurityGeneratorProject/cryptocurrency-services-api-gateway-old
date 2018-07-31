0c062b5ee131c071dba682f3064760acde5c1f43
kevin <kjwilde@hotmail.com>
2018-07-30 21:11:35 -0500

    May have worked around jenkins build issues.

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@
 
 #kubectl --namespace cryptocurrency-services delete pods -l app=cryptocurrency-services-api-gateway-cryptocurrency-services-api
 
-kubectl --namespace default delete pods -l app=cryptocurrency-services-api-gateway-cryptocurrency-services-api
+#kubectl --namespace default delete pods -l app=cryptocurrency-services-api-gateway-cryptocurrency-services-api
 
 
-kubectl create namespace cryptocurrency-services
+#kubectl create namespace cryptocurrency-services
 
 #helm --namespace cryptocurrency-services install -n cryptocurrency-services-api-gateway helm-charts/gateway
 
