98f091796d1761bd1a27e3ca4ca0984add082bdd
kevin <kjwilde@hotmail.com>
2018-07-31 15:36:16 -0500

    Scripting build and deployment.

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 buildEnv=$1
 mavenProfile=$2
 
-eval $(minikube docker-env)
-
 case ${buildEnv} in
   local)
         echo "build local"
+        eval $(minikube docker-env)
         export MONGO_HOST_TEST="localhost"
         export MONGO_PORT_TEST=0
         export MONGO_HOST="gateway-db-mongodb.default.svc.cluster.local"
