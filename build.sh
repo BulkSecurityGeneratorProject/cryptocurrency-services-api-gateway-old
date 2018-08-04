4ae13936e6a1deb7b3d8fef6c50efff2422288a9
kevin <kjwilde@hotmail.com>
2018-08-04 18:34:35 -0500

    Testing push to github.

--- build.sh
+++ build.sh
@@ -3,22 +3,37 @@
 buildEnv=$1
 mavenProfile=$2
 
+mongoHost="gateway-db-mongodb.default.svc.cluster.local"
+mongoPort="27017"
+mongoDatabase="CryptocurrencyServicesApiGateway"
+
+if [ ${KUBE_ENV} == "" ]
+then
+    mongoHost="ds113122.mlab.com"
+    mongoPort="13122"
+    mongoDatabase="cryptocurrency-services-prod-test"
+fi
+
 case ${buildEnv} in
   local)
         echo "build local"
         eval $(minikube docker-env)
         export MONGO_HOST_TEST="localhost"
         export MONGO_PORT_TEST=0
-        export MONGO_HOST="gateway-db-mongodb.default.svc.cluster.local"
-        export MONGO_PORT="27017"
+        export MONGO_DATABASE_TEST=${mongoDatabase}
+        export MONGO_HOST=${mongoHost}
+        export MONGO_PORT=${mongoPort}
+        export MONGO_DATABASE=${mongoDatabase}
         mvn -e -P${mavenProfile},${buildEnv} clean install dockerfile:build
         ;;
   container)
         echo "build container"
-        export MONGO_HOST_TEST="gateway-db-mongodb.default.svc.cluster.local"
-        export MONGO_PORT_TEST="27017"
-        export MONGO_HOST="gateway-db-mongodb.default.svc.cluster.local"
-        export MONGO_PORT="27017"
+        export MONGO_HOST_TEST=${mongoHost}
+        export MONGO_PORT_TEST=${mongoPort}
+        export MONGO_DATABASE_TEST=${mongoDatabase}
+        export MONGO_HOST=${mongoHost}
+        export MONGO_PORT=${mongoPort}
+        export MONGO_DATABASE=${mongoDatabase}
 #        mvn -e -P${mavenProfile} -s /host-home/.m2/settings.xml -Dmaven.repo.local=/host-home/.m2/repository clean verify dockerfile:build
         mvn -e -P${mavenProfile} ${SETTINGS_XML} ${MAVEN_REPO} clean verify dockerfile:build
         ;;
--- scripts/jenkins/jenkins-secrets-install.sh
+++ scripts/jenkins/jenkins-secrets-install.sh
@@ -4,9 +4,16 @@
 context=$1
 mongoProdTestUser=$2
 mongoProdTestPass=$3
-mongoProdTestUserPass=`echo "${mongoProdTestUser}:${mongoProdTestPass}@" | base64`
 
-echo "encrypted mongoProdTestUserPass: ${mongoProdTestUserPass}"
+mongoProdTestUserPass=""
+
+if [[ ("${mongoProdTestUser}"!="") && ("${mongoProdTestPass}" != "") ]]
+then
+    mongoProdTestUserPass=`echo "${mongoProdTestUser}:${mongoProdTestPass}@" | base64`
+fi
+
+
+echo "mongoProdTestUserPass: ${mongoProdTestUserPass}"
 
 cat ./jenkins-secrets.yml | sed "s/\X_MONGO_PROD_TEST_USER_PASS_X/${mongoProdTestUserPass}/" | kubectl --context=${context} --namespace jx create -f -
 
