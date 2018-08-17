b4321a4a6dfa2571a171e52daf2f790de1722796
kevin <kjwilde@hotmail.com>
2018-08-17 11:08:16 -0500

    Using skaffold to handle docker commands.

--- scripts/jenkins/jenkins-secrets-install.sh
+++ scripts/jenkins/jenkins-secrets-install.sh
@@ -4,20 +4,27 @@
 context=$1
 mongoProdTestUser=$2
 mongoProdTestPass=$3
+springDataMongodbUri=$3
 
 if [[ ("${mongoProdTestUser}"!="") && ("${mongoProdTestPass}" != "") ]]
 then
     mongoProdTestUser=`echo -n "${mongoProdTestUser}" | base64`
     mongoProdTestPass=`echo -n "${mongoProdTestPass}" | base64`
+    springDataMongodbUri=`echo -n "${springDataMongodbUri}" | base64`
 else
     mongoProdTestUser=""
     mongoProdTestPass=""
+    springDataMongodbUri=""
 fi
 
 echo "mongoProdTestUser: ${mongoProdTestUser}"
 echo "mongoProdTestPass: ${mongoProdTestPass}"
+echo "springDataMongodbUri: ${springDataMongodbUri}"
 
-cat ./jenkins-secrets.yml | sed "s/\X_MONGO_PROD_TEST_USER_X/${mongoProdTestUser}/" | sed "s/\X_MONGO_PROD_TEST_PASS_X/${mongoProdTestPass}/" | kubectl --context=${context} --namespace jx create -f -
+cat ./jenkins-secrets.yml | sed "s/\X_MONGO_PROD_TEST_USER_X/${mongoProdTestUser}/" | \
+sed "s/\X_MONGO_PROD_TEST_PASS_X/${mongoProdTestPass}/" | \
+sed "s/\X_SPRING_DATA_MONGODB_URI_X/${springDataMongodbUri}/" | \
+kubectl --context=${context} --namespace jx create -f -
 
 
 
